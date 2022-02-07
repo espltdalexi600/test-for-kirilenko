fcce437f26e317f0dd573a08e3fba767f7dab665
espltdalexi600 <ew.petrashevitch@gmail.com>
2022-02-07 18:12:19 +0300

    Add section 3.

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,18 @@ module.exports = {
           from: path.resolve(__dirname, 'src/img/tv.png'),
           to: path.resolve(__dirname, 'dist'),
         },
+        {
+          from: path.resolve(__dirname, 'src/img/list_1.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/img/list_2.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/img/list_3.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
       ],
     }),
   ],
